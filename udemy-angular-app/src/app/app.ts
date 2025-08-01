47defe515e06c567a3942822cc6825d0babec6e4
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2025-08-01 20:36:21 +0300

    feat: add header components

--- udemy-angular-app/src/app/app.ts
+++ udemy-angular-app/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class AppComponent {
   protected readonly title = signal('udemy-angular-app');
 }
