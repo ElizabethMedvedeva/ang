51b81ce2a52644c429fc23dcd9a6a09bc104a0e7
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2025-08-07 17:45:17 +0300

    feat: add variation of data interpolation

--- udemy-angular-app/src/app/user/user.ts
+++ udemy-angular-app/src/app/user/user.ts
@@ -1,7 +1,5 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, computed, input, Input } from '@angular/core';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -10,12 +8,24 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() =>  'assets/users/' + this.selectedUser().avatar)
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
 
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser.set(DUMMY_USERS[randomIndex])
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
+
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // })
+  // это для использования через signal, 
+  // но дальше мы использовать не будем такую версию,
+  // так как вариант для часто используемых и 
+  // старых вариантов через декоратор (смотри выше, лекция 32)
+
+  onSelectUsers() { }
 }
